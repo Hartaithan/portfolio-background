d0e179856d844a904382bf8a4f0626a719808f6e
Hartaithan <hartaithan@gmail.com>
2024-12-29 14:51:51 +0900

    keep init in main file

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,25 @@
-import { init } from "./init";
+import { initializeThree } from "./three";
+import { createScene } from "./scene";
+import { createCamera } from "./camera";
+import { createRenderer } from "./renderer";
+import { createFigure } from "./figure";
+import { getContainer, setupContainer } from "./container";
+
+export const init = async () => {
+  await initializeThree();
+
+  const container = getContainer();
+  const scene = createScene();
+  const camera = createCamera();
+  const renderer = createRenderer();
+
+  setupContainer(container, renderer);
+
+  const figure = createFigure();
+  scene.add(figure);
+
+  renderer.render(scene, camera);
+};
 
 if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", init);
