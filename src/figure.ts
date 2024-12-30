d5dfde1d88f9d5776db9caf0f05f1f45f66eab4a
Hartaithan <hartaithan@gmail.com>
2024-12-30 13:43:46 +0900

    add wireframe material

--- src/figure.ts
+++ src/figure.ts
@@ -1,9 +1,17 @@
-import { Mesh } from "three";
+import { Mesh, MeshStandardMaterialParameters } from "three";
 import { getThree } from "./three";
+
+const materialParameters: MeshStandardMaterialParameters = {
+  color: "#ffffff",
+  wireframe: true,
+  transparent: true,
+  opacity: 0.3,
+};
 
 export const createFigure = (): Mesh => {
   const THREE = getThree();
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial();
-  return new THREE.Mesh(geometry, material);
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial(materialParameters);
+  const mesh = new THREE.Mesh(geometry, material);
+  return mesh;
 };
