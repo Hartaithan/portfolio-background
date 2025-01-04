3d4613883faec7245702b3afdf212adbf45300b0
Hartaithan <hartaithan@gmail.com>
2025-01-04 13:10:29 +0900

    load font based on figure type

---
+++ src/font.ts
@@ -0,0 +1,31 @@
+import { FontLoader, Font as ThreeFont } from "three/examples/jsm/Addons.js";
+import { Figure } from "./figure";
+
+export class Font {
+  private static font: ThreeFont;
+
+  constructor() {}
+
+  public static async load() {
+    if (Figure.getType() !== "text") return;
+    return new Promise<ThreeFont>((resolve, reject) => {
+      const loader = new FontLoader();
+      loader.load(
+        "arial.json",
+        (font) => {
+          Font.font = font;
+          resolve(font);
+        },
+        undefined,
+        (error) => {
+          console.log("font loader error", error);
+          reject(error);
+        }
+      );
+    });
+  }
+
+  public static get() {
+    return Font.font;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { Scene } from "./scene";
 import { Camera } from "./camera";
 import { Renderer } from "./renderer";
 import { initializeAnimation } from "./animation";
+import { Font } from "./font";
 
 export const init = async () => {
   Figure.pick();
+  await Font.load();
   Scene.create();
   Camera.create();
   Renderer.create();
